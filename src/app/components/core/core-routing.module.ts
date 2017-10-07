db8837eacfee00ce4077059dd7a7d341346cdbb6
bhanu thareja <jimmythareja@icloud.com>
2017-10-07 16:05:18 +0300

    body compoenent added

--- src/app/components/core/core-routing.module.ts
+++ src/app/components/core/core-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
+import { BodyComponent } from './body/body.component';
 
 const appRoutes: Routes = [
-    {path: '', component: HomeComponent}
+    {path: '', component: BodyComponent}
 ]
 
 @NgModule({
--- src/app/components/core/core.module.ts
+++ src/app/components/core/core.module.ts
@@ -2,19 +2,19 @@ import { CoreRoutingModule } from './core-routing.module';
 import { NgModule } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
       HeaderComponent, 
-      HomeComponent
+      BodyComponent
     ],
     imports: [
         CoreRoutingModule
     ],
     exports: [
         HeaderComponent,
-        HomeComponent,
+        BodyComponent,
         CoreRoutingModule
     ]
 })
